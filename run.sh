fcf132b48587e87e5d25032d10a217d3030390f7
zylozs <vincent.loignon@gmail.com>
2022-04-23 16:37:56 -0400

    - Updated run.sh to loop so we can remotely restart the bot using commands

--- run.sh
+++ run.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-rm -rf jppbot
-git clone https://github.com/zylozs/jppbot.git
-cd jppbot
-pip3 install -r requirements.txt
+while :
+do
+	rm -rf jppbot
+	git clone https://github.com/zylozs/jppbot.git
+	cd jppbot
+	pip3 install -r requirements.txt
 
-python3 jppbot.py --ip=$1 --port=$2 --token=$3
+	python3 jppbot.py --ip=$1 --port=$2 --token=$3
+	cd ..
+done
