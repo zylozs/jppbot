f5aa022ac03c9bff34a21fcec42d467c22c9d5b4
zylozs <vincent.loignon@gmail.com>
2022-08-31 22:16:35 -0400

    - Updated the run.sh script so that when it tries to install the python packages, it will grab the latest versions.

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ do
 	rm -rf jppbot
 	git clone https://github.com/zylozs/jppbot.git
 	cd jppbot
-	pip3 install -r requirements.txt
+	pip3 install -U -r requirements.txt
 
 	python3 jppbot.py --ip=$1 --port=$2 --token=$3
 	cd ..
